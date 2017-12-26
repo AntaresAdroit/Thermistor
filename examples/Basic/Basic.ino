5087211d0130d475e8b32748c1975ca21419441b
AntaresAdroit <AntaresAdroit@gmail.com>
2017-12-25 20:41:00 -0800

    Update Basic.ino
    Cleaned up the code (removed some old code not relevant to the example). Also, added comments.

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,5 +1,12 @@
+//Basic demonstration of how to use the library
+//Takes measurements from 2 separate thermistors and
+//prints them to the serial port. Each reading is an
+//average of 10 measurements.
+
 #include <Thermistor.h>
 
+//Create two thermistors, one on pin A0 and one on pin A1
+//Parameters: (analog pin, fixed resistor value, ka, kb, kx2, kc)
 Thermistor bath(A0, 10000, 7.59274E-04, 2.14411E-04, -5.47089E-07, 1.29089E-07);
 Thermistor air(A1, 10000, 1.85776E-03, 2.12327E-05, 2.27316E-05, -7.74693E-07);
 
@@ -9,9 +16,6 @@ void setup() {
 }
 
 void loop() {
-  double t;
-  //t = bath.avgThermistor(2);
-  //t = air.avgThermistor(2);
   Serial.print(bath.avgThermistor(10));
   Serial.print(" ");
   Serial.println(air.avgThermistor(10));
