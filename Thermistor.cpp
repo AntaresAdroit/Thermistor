d9f6e15d06ec4620278bd7c157866e40be8f6a65
AntaresAdroit <AntaresAdroit@gmail.com>
2017-12-25 20:29:52 -0800

    Update Thermistor.cpp
    Added comments. Changed the name of the input parameter in the avgThermistor() function.

--- Thermistor.cpp
+++ Thermistor.cpp
@@ -12,23 +12,31 @@
     _rH = rH;
   }
   
-  
+// Public Methods //////////////////////////////////////////////////////////////  
+
+  //Returns the resistance of a thermistor given a double value (from 0 to 1023)
+  //represinting the fraction of Vref at the input pin. This is mainly a helper
+  //function for tempC()
   double Thermistor::rThermistor(double rawReading){
     double voltageFraction;
     voltageFraction = ( rawReading )/1023.0;
     return voltageFraction * _rH / ( 1.0 - voltageFraction);
   }
-
+  
+  //Returns the temperature of a thermistor in C given a double value (from 0 to 1023)
+  //represinting the fraction of Vref at the input pin. 
   double Thermistor::tempC(double rawReading){
     double lnR = log(rThermistor(rawReading));
     return 1 / ( _ka + (_kb * lnR) + (_kx2 * lnR * lnR) + (_kc * lnR * lnR * lnR) ) - 273.15;
   }
 
-  double Thermistor::avgThermistor(uint16_t count){
+  //Returns the temperature of a thermistor in C from multiple readings of the analog input
+  //pin. The number of readings is specified by the samples parameter.
+  double Thermistor::avgThermistor(uint16_t samples){
     uint32_t avgTempC;                 
     avgTempC = 0;
-    for(uint16_t i = 0; i < count; i++){
+    for(uint16_t i = 0; i < samples; i++){
       avgTempC = avgTempC + analogRead(_pin);
     }
-    return tempC(avgTempC / double(count));
+    return tempC(avgTempC / double(samples));
   }
